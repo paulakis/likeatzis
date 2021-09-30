e5c5126fe19bf149967b41263531356116ebb1e6
unknown <p.pavlou@mpass.gr>
2021-09-30 16:45:13 +0300

    first commit

---
+++ src/main/java/self/company/likeatzis/LikeatzisApplication.java
@@ -0,0 +1,13 @@
+package self.company.likeatzis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeatzisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeatzisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self/company/likeatzis/LikeatzisApplicationTests.java
@@ -0,0 +1,13 @@
+package self.company.likeatzis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeatzisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
